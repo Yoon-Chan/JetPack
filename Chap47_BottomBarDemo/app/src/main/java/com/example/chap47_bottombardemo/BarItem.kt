34e32565ad76d29c0abac4c34c4162ba8a30b83c
Yoon Chan <vsvx13@naver.com>
2023-07-04 17:29:03 +0900

    Chap47 BarItem을 이용한 리스트를 갖고 있는 NavBarItems object 생성

--- Chap47_BottomBarDemo/app/src/main/java/com/example/chap47_bottombardemo/BarItem.kt
+++ Chap47_BottomBarDemo/app/src/main/java/com/example/chap47_bottombardemo/BarItem.kt
@@ -2,4 +2,5 @@ package com.example.chap47_bottombardemo
 
 import androidx.compose.ui.graphics.vector.ImageVector
 
+//하단 바에 필요한 BarItem 데이터 클래스 생성
 data class BarItem(val title : String, val image : ImageVector, val route : String)
---
+++ Chap47_BottomBarDemo/app/src/main/java/com/example/chap47_bottombardemo/NavBarItems.kt
@@ -0,0 +1,27 @@
+package com.example.chap47_bottombardemo
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+
+object NavBarItems {
+    val BarItems = listOf(
+        BarItem(
+            title = "Home",
+            image = Icons.Filled.Home,
+            route = "home"
+        ),
+        BarItem(
+            title = "Contacts",
+            image = Icons.Filled.Face,
+            route = "contacts"
+        ),
+        BarItem(
+            title = "Home",
+            image = Icons.Filled.Favorite,
+            route = "favorites"
+        )
+    )
+
+}
