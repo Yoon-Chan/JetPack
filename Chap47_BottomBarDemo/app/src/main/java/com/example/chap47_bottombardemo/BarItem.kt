2594fb3e61d0a6d07bf1277b243caafe5d8dcb29
Yoon Chan <vsvx13@naver.com>
2023-07-04 17:25:10 +0900

    Chap47 바 아이템 디자인하기 위한 BarItem인 data class 생성

---
+++ Chap47_BottomBarDemo/app/src/main/java/com/example/chap47_bottombardemo/BarItem.kt
@@ -0,0 +1,5 @@
+package com.example.chap47_bottombardemo
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BarItem(val title : String, val image : ImageVector, val route : String)
