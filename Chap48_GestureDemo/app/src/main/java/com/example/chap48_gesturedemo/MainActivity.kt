4588dfab4fdc7d5f8980721738d24805223bf75e
Yoon Chan <vsvx13@naver.com>
2023-07-05 10:38:42 +0900

    chap48 Modifier.clickable 사용 예제 ClickDemo 함수 구현

--- Chap48_GestureDemo/app/src/main/java/com/example/chap48_gesturedemo/MainActivity.kt
+++ Chap48_GestureDemo/app/src/main/java/com/example/chap48_gesturedemo/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.chap48_gesturedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.chap48_gesturedemo.ui.theme.Chap48_GestureDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,12 +42,35 @@ class MainActivity : ComponentActivity() {
 }
 
 
-
 @Composable
 fun MainScreen() {
-
+    ClickDemo()
 }
 
+@Composable
+fun ClickDemo() {
+    var colorState by remember {
+        mutableStateOf(true)
+    }
+    var bgColor by remember {
+        mutableStateOf(Color.Blue)
+    }
+
+    val clickHandler = {
+        colorState = !colorState
+
+        bgColor = if (colorState) {
+            Color.Blue
+        } else {
+            Color.DarkGray
+        }
+    }
+
+    Box(modifier = Modifier
+        .clickable { clickHandler() }
+        .background(bgColor)
+        )
+}
 
 @Preview(showBackground = true)
 @Composable
